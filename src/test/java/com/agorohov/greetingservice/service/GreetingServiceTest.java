263595c57f92b442e50382b30d752f9f10ab995c
agorohov <aleksandr.gorohov@gmail.com>
2024-05-15 22:28:56 +0300

    init

--- src/test/java/com/agorohov/greetingservice/service/GreetingServiceTest.java
+++ src/test/java/com/agorohov/greetingservice/service/GreetingServiceTest.java
@@ -1,6 +1,5 @@
 package com.agorohov.greetingservice.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -9,42 +8,44 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
 
 import static com.agorohov.greetingservice.constants.GreetingServiceTestConstants.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingServiceTest {
 
     private final GreetingService out = new GreetingService();
 
     @Test
+//    @Disabled - отключает тест
     public void shouldReturnDefaultMessageWhenNameIsNoExists() {
         String result = out.generateGreetings();
-        Assertions.assertTrue(result.contains(DEFAULT_NAME));
-        Assertions.assertEquals(DEFAULT_MESSAGE, result);
+        assertTrue(result.contains(DEFAULT_NAME));
+        assertEquals(DEFAULT_MESSAGE, result);
     }
 
     @Test
     public void shouldReturnDefaultMessageWhenNameIsNull() {
         String result = out.generateGreetings(null);
-        Assertions.assertTrue(result.contains(DEFAULT_NAME));
-        Assertions.assertEquals(DEFAULT_MESSAGE, result);
+        assertTrue(result.contains(DEFAULT_NAME));
+        assertEquals(DEFAULT_MESSAGE, result);
     }
 
     @Test
     public void shouldReturnDefaultMessageWhenNameIsEmpty() {
         String result = out.generateGreetings(EMPTY_NAME);
-        Assertions.assertTrue(result.contains(DEFAULT_NAME));
-        Assertions.assertEquals(DEFAULT_MESSAGE, result);
+        assertTrue(result.contains(DEFAULT_NAME));
+        assertEquals(DEFAULT_MESSAGE, result);
     }
 
     @Test
     public void shouldReturnDefaultMessageWhenNameIsSpace() {
         String result = out.generateGreetings(ONLY_SPACES_NAME);
-        Assertions.assertTrue(result.contains(DEFAULT_NAME));
-        Assertions.assertEquals(DEFAULT_MESSAGE, result);
+        assertTrue(result.contains(DEFAULT_NAME));
+        assertEquals(DEFAULT_MESSAGE, result);
     }
 
     @Test
     public void shouldThrowIIllegalArgumentExceptionWhenNameContainsIllegalCharacters() {
-        Assertions.assertThrows(IllegalArgumentException.class,
+        assertThrows(IllegalArgumentException.class,
                 () -> out.generateGreetings(ILLEGAL_CHARS_NAME)
         );
     }
@@ -52,22 +53,22 @@ class GreetingServiceTest {
     @Test
     public void shouldReturnCorrectMessageWhenNameIsCorrect() {
         String result = out.generateGreetings(CORRECT_NAME);
-        Assertions.assertTrue(result.contains(CORRECT_NAME));
-        Assertions.assertEquals(CORRECT_MESSAGE, result);
+        assertTrue(result.contains(CORRECT_NAME));
+        assertEquals(CORRECT_MESSAGE, result);
     }
 
     @Test
     public void shouldReturnCorrectMessageWhenNameIsInUpperCase() {
         String result = out.generateGreetings(UPPER_CASE_NAME);
-        Assertions.assertTrue(result.contains(CORRECT_NAME));
-        Assertions.assertEquals(CORRECT_MESSAGE, result);
+        assertTrue(result.contains(CORRECT_NAME));
+        assertEquals(CORRECT_MESSAGE, result);
     }
 
     @Test
     public void shouldReturnCorrectMessageWhenNameIsInLowerCase() {
         String result = out.generateGreetings(LOWER_CASE_NAME);
-        Assertions.assertTrue(result.contains(CORRECT_NAME));
-        Assertions.assertEquals(CORRECT_MESSAGE, result);
+        assertTrue(result.contains(CORRECT_NAME));
+        assertEquals(CORRECT_MESSAGE, result);
     }
 
     // Параметризованные тесты
@@ -84,9 +85,12 @@ class GreetingServiceTest {
 
     @ParameterizedTest
     @MethodSource("provideParamsForTest")
+    // Если мы не указываем имя для @MethodSource,
+    // JUnit будет искать исходный метод с тем же именем, что и тестовый метод.
+    // Есть и другие варианты, например: @ValueSource(), @NullAndEmptySource, @EnumSource и другие.
     public void shouldGenerateCorrectMessages(String inputName, String messageName, String expectedMessage) {
         String result = out.generateGreetings(inputName);
-        Assertions.assertTrue(result.contains(messageName));
-        Assertions.assertEquals(expectedMessage, result);
+        assertTrue(result.contains(messageName));
+        assertEquals(expectedMessage, result);
     }
 }
